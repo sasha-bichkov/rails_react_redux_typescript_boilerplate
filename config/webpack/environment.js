676c278571daf96d6dd7c6f8605443f312e66ba7
Sasha Bychkov <aleksandar.bichkov@gmail.com>
2022-01-08 17:50:25 +0500

    Added Typescript

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,11 @@
+const path = require('path')
 const { environment } = require('@rails/webpacker')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+
+environment.plugins.prepend('ForkTsCheckerWebpackPlugin', new ForkTsCheckerWebpackPlugin({
+  typescript: {
+    configFile: path.resolve('tsconfig.json')
+  }
+}))
 
 module.exports = environment
