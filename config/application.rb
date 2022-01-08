a040aef41365e6fcc50b2c9e4a3fe5563bc7623b
Sasha Bychkov <aleksandar.bichkov@gmail.com>
2022-01-08 11:31:56 +0500

    Fixed all rubocop offenses

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
-require_relative "boot"
+# frozen_string_literal: true
 
-require "rails/all"
+require_relative 'boot'
+
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -8,6 +10,7 @@ Bundler.require(*Rails.groups)
 
 module RailsReactReduxTypescriptBoilerplate
   class Application < Rails::Application
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
@@ -18,5 +21,6 @@ module RailsReactReduxTypescriptBoilerplate
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
   end
 end
